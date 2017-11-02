3f64fa07a953c3103b63e8cd9f0362792f0e0000
Manson Xasthur <mansonx13@gmail.com>
2017-11-02 15:28:24 +0200

    second initial

--- app/Http/Controllers/ArtistController.php
+++ app/Http/Controllers/ArtistController.php
@@ -20,7 +20,7 @@ class ArtistController extends Controller
 
     public function store(Request $request)
     {
-        $root = 'http://' . $_SERVER['SERVER_NAME'] . ':89/test/public';
+        $root = 'http://' . $_SERVER['SERVER_NAME'] . ':89/elmoledcpanel/public';
         $this->validate($request, [
            'artist_name' => 'required'
         ]);
@@ -97,7 +97,7 @@ class ArtistController extends Controller
         $artist = Artist::find($request->id);
         $artist->artist_name = $request->artist_name;
         if ($request->hasFile('img_url')){
-            $root = 'http://' . $_SERVER['SERVER_NAME'] . ':89/test/public';
+            $root = 'http://' . $_SERVER['SERVER_NAME'] . ':89/elmoledcpanel/public';
             $file = $request->file('img_url');
             $destinationPath = 'img/artist';
             $file->move($destinationPath, $file->getClientOriginalName());
